7bde3ccc58c68b936b4d96d1d2a2cb22adbb7299
IPSBHANGU <inderpreetbhangu2@gmail.com>
2024-05-22 12:18:31 +0530

    Revert "fuxi: Add support for DC Dimming"
    This reverts commit c0882d6a838aa3d6b30d86be1506146bdda45969.

--- parts/src/com/xiaomi/settings/BootCompletedReceiver.java
+++ parts/src/com/xiaomi/settings/BootCompletedReceiver.java
@@ -17,7 +17,6 @@ import android.view.Display;
 import android.view.Display.HdrCapabilities;
 
 import com.xiaomi.settings.display.ColorModeService;
-import com.xiaomi.settings.display.DcDimmingService;
 import com.xiaomi.settings.doze.AodBrightnessService;
 import com.xiaomi.settings.touch.TapToWakeService;
 import com.xiaomi.settings.touch.TouchOrientationService;
@@ -47,8 +46,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
         // Display
         context.startServiceAsUser(new Intent(context, ColorModeService.class),
-                UserHandle.CURRENT);
-        context.startServiceAsUser(new Intent(context, DcDimmingService.class),
                 UserHandle.CURRENT);
 
         // Touchscreen
