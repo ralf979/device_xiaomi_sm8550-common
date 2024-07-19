4d29646fe29672f3fe7e6802c95075a27490dd5f
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-07-19 10:58:43 +0200

    sm8550-common: Drop aod auto brightness service
    This reverts commit 9413ad972bb0c0f24fda0e7d55bfadcbfa70a881.
    
    Change-Id: Ic3ce84bb295bde37c490a40a0dae292bb95aaac7

--- parts/src/com/xiaomi/settings/BootCompletedReceiver.java
+++ parts/src/com/xiaomi/settings/BootCompletedReceiver.java
@@ -17,7 +17,6 @@ import android.view.Display;
 import android.view.Display.HdrCapabilities;
 
 import com.xiaomi.settings.display.ColorModeService;
-import com.xiaomi.settings.doze.AodBrightnessService;
 import com.xiaomi.settings.touch.TapToWakeService;
 import com.xiaomi.settings.touch.TouchOrientationService;
 
@@ -40,10 +39,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     }
 
     private static void onLockedBootCompleted(Context context) {
-        // AOD
-        context.startServiceAsUser(new Intent(context, AodBrightnessService.class),
-                UserHandle.CURRENT);
-
         // Display
         context.startServiceAsUser(new Intent(context, ColorModeService.class),
                 UserHandle.CURRENT);
