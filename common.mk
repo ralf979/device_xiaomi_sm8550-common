d5333b110fdc645c39e79fb3107579d6f0449b90
IPSBHANGU <inderpreetbhangu2@gmail.com>
2024-08-04 07:07:55 +0200

    sm8550-common: build missing libusbhost

--- common.mk
+++ common.mk
@@ -603,6 +603,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
+PRODUCT_PACKAGES += \
+    libusbhost.vendor
+
 # Vendor service manager
 PRODUCT_PACKAGES += \
     vndservicemanager
